68391bcb3e11a247060916beac41ebd08cab4959
Michael DOise <mikedoise@icloud.com>
2022-10-26 15:51:14 -0500

    Initial Commit

---
+++ iAccessibility/iAccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAccessibilityApp.swift
+//  iAccessibility
+//
+//  Created by Michael Doise on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct iAccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
